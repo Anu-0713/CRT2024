f92297d921a39ec51be76d46eccff04c11b8b3d6
Anu-0713 <136591876+Anu-0713@users.noreply.github.com>
2024-06-11 12:25:00 +0530

    Maximum_element in array

---
+++ Maximum_element.c
@@ -0,0 +1,35 @@
+/*Write a program to find the maximum element in an array.
+
+Input Format
+
+Integer corresponds to the size of the array. Read n elements line by line
+
+Constraints
+
+1<=n<=10^7
+
+Output Format
+
+Print maximum value
+
+Sample Input 0
+
+5
+1
+2
+33
+4
+5
+Sample Output 0
+
+33
+Sample Input 1
+
+3
+10
+210
+90
+Sample Output 1
+
+210
+*/
